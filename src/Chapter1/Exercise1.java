1e2bd4f61d6386395393d598480590263ab159a5
Warren Zhang <warren@cincoai.com>
2019-08-05 23:42:03 -0700

    setup some directories

---
+++ src/Chapter1/Exercise1.java
@@ -0,0 +1,10 @@
+package Chapter1;
+
+/**
+ * Created by GelinZHU on 8/5/19.
+ */
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.print("Hello, World");
+    }
+}
